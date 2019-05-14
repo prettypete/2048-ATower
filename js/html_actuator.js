61631a8711e79ebf72722733189901e0aeac8085
Peter Amenewolde <peterame18@gmail.com>
2019-05-14 01:38:20 -0400

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -173,7 +173,7 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   tweet.setAttribute("href", "https://twitter.com/share");
   tweet.setAttribute("data-via", "giampiex");
   tweet.setAttribute("data-url", "http://git.io/simatowah");
-  tweet.setAttribute("data-counturl", "http://prettypete.github.io/2048-simatowah");
+  tweet.setAttribute("data-counturl", "http://prettypete.github.io/2048-Atower");
   tweet.textContent = "Tweet";
 
   var text = Localize("tweet1") + Localize( this.score ).toUpperCase() + '", ' + this.points + " strength " + Localize("tweet2");
